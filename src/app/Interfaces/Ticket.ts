beab1c398723c578a2f7ad7820b5dd13545640c7
Sebastian Ramirez <sebastian.rv.cb.2001@outlook.com>
2023-06-03 14:53:43 -0500

    feat: cambios en el dialog para el calculo de tickets

---
+++ src/app/Interfaces/Ticket.ts
@@ -0,0 +1,11 @@
+export interface Ticket{
+    id: string,
+    ticket: string,
+    cantidad: string,
+    undmedida: string,
+    descripcion: string,
+    preciou: string,
+    igv: string,
+    descuento: string,
+    importe: string
+}
--- src/app/shared/acopio-creation-dialog/acopio-creation-dialog.component.ts
+++ src/app/shared/acopio-creation-dialog/acopio-creation-dialog.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import {MatTableDataSource} from '@angular/material/table';
+import { MatTableDataSource } from '@angular/material/table';
 import { Observable, map, startWith } from 'rxjs';
 import {
   trigger,
@@ -11,34 +11,17 @@ import {
 } from '@angular/animations';
 import { PartnerService } from 'src/app/services/partner.service';
 import { GatheringService } from 'src/app/services/gathering.service';
+import { Ticket } from 'src/app/Interfaces/Ticket';
+import { TicketService } from 'src/app/services/ticket.service';
 export interface DatosProducto {
   producto: string;
   codigo: string;
 }
 
 const dataproducto: DatosProducto[] = [
-  {codigo: 'RFF', producto: 'RACIMO DE FRUTA FRESCA DE P.A'},
-  {codigo: 'FS', producto: 'FRUTO SUELTO DE P.A'},
+  { codigo: 'RFF', producto: 'RACIMO DE FRUTA FRESCA DE P.A' },
+  { codigo: 'FS', producto: 'FRUTO SUELTO DE P.A' },
 ];
-
-export interface DatosTicket {
-  ticket: number;
-  cantidad: number;
-  undmedida: string;
-  descripcion: string;
-  preciou: number;
-  igv: number;
-  descuento: number;
-  importe:number;
-}
-
-const dataticket: DatosTicket[] = [
-  {ticket:1, cantidad: 12, undmedida:'KILOS', descripcion:'Se ordenaron muchos kilos', preciou: 12, igv: 0, descuento: 0, importe: 12},
-  {ticket:2, cantidad: 12, undmedida:'KILOS', descripcion:'Se ordenaron muchos kilos', preciou: 12, igv: 0, descuento: 0, importe: 12},
-  {ticket:3, cantidad: 12, undmedida:'KILOS', descripcion:'Se ordenaron muchos kilos', preciou: 12, igv: 0, descuento: 0, importe: 12},
-  {ticket:4, cantidad: 12, undmedida:'KILOS', descripcion:'Se ordenaron muchos kilos', preciou: 12, igv: 0, descuento: 0, importe: 12},
-];
-
 
 @Component({
   selector: 'app-acopio-creation-dialog',
@@ -54,12 +37,21 @@ const dataticket: DatosTicket[] = [
   ],
 })
 export class AcopioCreationDialogComponent implements OnInit {
-
-  displayedColumns: string[] = ['codigo', 'producto'];  
-  displayedColumns2: string[] = ['ticket', 'cantidad', 'undmedida', 'descripcion', 'preciou', 'igv', 'descuento', 'importe'];  
+  inputDisabled = false; //VARIBLE PARA DESAHABILITAR LOS INPUTS
+  displayedColumns: string[] = ['codigo', 'producto'];
+  displayedColumns2: string[] = ['ticket', 'cantidad', 'undmedida', 'descripcion', 'preciou', 'igv', 'descuento', 'importe'];
   dataSource = new MatTableDataSource(dataproducto);
-  dataSource2 = new MatTableDataSource(dataticket);
-  list:FormGroup;
+  dataSource2 = new MatTableDataSource<Ticket>();
+  list: FormGroup;
+  //OPCIONES FILTRADAS PARA AUTOCOMPLETAR
+  filteredOptions!: Observable<string[]>;
+  filteredOptions2!: Observable<string[]>;
+  filteredOptions3!: Observable<string[]>;
+  filteredOptions4!: Observable<string[]>;
+  filteredOptions5!: Observable<string[]>;
+  filteredOptions6!: Observable<string[]>;
+  //FIN DE AUTOCOMPLETAR
+  //VARIABLES DE CADA INPUT
   selectedOption: any;
   selectedOption2: any;
   selectedOption3: any;
@@ -67,27 +59,22 @@ export class AcopioCreationDialogComponent implements OnInit {
   selectedOption5: any;
   selectedCompra: any;
   selectedVenta: any;
-  filteredOptions!: Observable<string[]>;
-  filteredOptions2!: Observable<string[]>;
-  filteredOptions3!: Observable<string[]>;
-  filteredOptions4!: Observable<string[]>;
-  filteredOptions5!: Observable<string[]>;
-  filteredOptions6!: Observable<string[]>;
-  selectedOption6: any;
-  selectedOption7: any;
-  selectedOption8: any;
-  selectedOption9: any;
-  selectedOption10: any;
-  selectedOption11: any;
-  selectedOption12: any;
-  selectedOption13: any;
+  precioDolarR: any;
+  precioSolesR: any;
+  cantidadAcopio: any;
+  precioDolar: any;
+  igvDolar: any;
+  importeDolares: any;
+  precioSoles: any;
+  igvSoles: any;
+  importeSoles: any;
   selectedOption14: any;
   selectedOption15: any;
   selectedOption16: any;
   selectedOption17: any;
-
-
-  constructor(private fb: FormBuilder, private service: PartnerService, private gatService: GatheringService) {
+  //FIN DE VARIABLES
+  constructor(private fb: FormBuilder, private service: PartnerService, private gatService: GatheringService, private ticket: TicketService) {
+    //INICIOFORMULARIO
     this.list = this.fb.group({
       codigoacopio: ['', Validators.required],
       centroacopio: ['', Validators.required],
@@ -103,20 +90,20 @@ export class AcopioCreationDialogComponent implements OnInit {
       ruc: ['', Validators.required],
       transportista: ['', Validators.required],
       fechacomprobante: [new Date().toISOString(), Validators.required],
-      formapago: ['', Validators.required],
-      moneda: ['', Validators.required],
+      formapago: ['CREDITO', Validators.required],
+      moneda: ['SOLES', Validators.required],
       fecharegistro: [new Date().toISOString(), Validators.required],
       compra: [3.676, Validators.required],
       venta: [3.684, Validators.required],
       flete: ['', Validators.required],
-      comprobante: ['', Validators.required],
+      comprobante: ['LIQUIDACION DE COMPRA ELECTRONICA', Validators.required],
       serie: ['', Validators.required],
       correlativo: ['', Validators.required],
       ticket: ['', Validators.required],
       cantidad: ['', Validators.required],
       preciodolarr: ['', Validators.required],
       preciosolesr: ['', Validators.required],
-      unidadm: ['', Validators.required],
+      unidadm: ['TN', Validators.required],
       preciodolar: ['', Validators.required],
       igvdolar: ['', Validators.required],
       descuentodolar: ['', Validators.required],
@@ -126,147 +113,208 @@ export class AcopioCreationDialogComponent implements OnInit {
       descuentosoles: ['', Validators.required],
       importesoles: ['', Validators.required],
     });
-}
-
-ngOnInit() {
-  this.loadOptions();
-}
-
-loadOptions() {
-  // Realiza la solicitud HTTP para obtener los datos del endpoint
-  this.gatService.getCenters().subscribe(data => {
-    // Mapea los datos de respuesta al formato de opciones que necesitas (id y value)
-    const options4 = data.map(item => item.centro);
-    
-    // Configura la lógica de filtrado y asigna las opciones filtradas a la variable
-    this.filteredOptions4 = this.list.controls['centroacopio'].valueChanges.pipe(
-      startWith(''),
-      map(value => this.filterOptions(value, options4))
-    );
-  });
-
-  this.gatService.getTransportistas().subscribe(data => {
-    // Mapea los datos de respuesta al formato de opciones que necesitas (id y value)
-    const options3 = data.map(item => item.transporte);
-    
-    // Configura la lógica de filtrado y asigna las opciones filtradas a la variable
-    this.filteredOptions3 = this.list.controls['transportista'].valueChanges.pipe(
-      startWith(''),
-      map(value => this.filterOptions(value, options3))
-    );
-  });
-
-  this.gatService.getCenters().subscribe(data => {
-    // Mapea los datos de respuesta al formato de opciones que necesitas (id y value)
-    const options = data.map(item => item.id);
-    
-    // Configura la lógica de filtrado y asigna las opciones filtradas a la variable
-    this.filteredOptions = this.list.controls['codigoacopio'].valueChanges.pipe(
-      startWith(''),
-      map(value => this.filterOptions(value, options))
-    );
-  });
-
-  this.service.getPartners().subscribe(data => {
-    // Mapea los datos de respuesta al formato de opciones que necesitas (id y value)
-    const options2 = data.map(item => item.id);
-    
-    // Configura la lógica de filtrado y asigna las opciones filtradas a la variable
-    this.filteredOptions2 = this.list.controls['codigosocio'].valueChanges.pipe(
-      startWith(''),
-      map(value => this.filterOptions(value, options2))
-    );
-  });
-  this.service.getPartners().subscribe(data => {
-    // Mapea los datos de respuesta al formato de opciones que necesitas (id y value)
-    const options5 = data.map(item => item.dni);
-    
-    // Configura la lógica de filtrado y asigna las opciones filtradas a la variable
-    this.filteredOptions5 = this.list.controls['numerodoc'].valueChanges.pipe(
-      startWith(''),
-      map(value => this.filterOptions(value, options5))
-    );
-  });
-  this.service.getPartners().subscribe(data => {
-    // Mapea los datos de respuesta al formato de opciones que necesitas (id y value)
-    const options6 = data.map(item => item.nombre);
-    
-    // Configura la lógica de filtrado y asigna las opciones filtradas a la variable
-    this.filteredOptions6 = this.list.controls['nombres'].valueChanges.pipe(
-      startWith(''),
-      map(value => this.filterOptions(value, options6))
-    );
-  });
-}
-
-filterOptions(value: string, options: string[]): string[] {
-  // Filtra las opciones basadas en el valor de entrada
-  const filterValue = value.toLowerCase();
-  return options.filter(option => option.toLowerCase().includes(filterValue));
-}
-
-applyFilter(event: Event) {
-  const filterValue = (event.target as HTMLInputElement).value;
-  this.dataSource.filter = filterValue.trim().toLowerCase();
-}
-
-onOptionSelected(event: any) {
-
-  this.gatService.getCentersById(event.option.value).subscribe(data => {
-    this.selectedOption = data.centro;
-    
-  });
-}
-onOptionSelected3(event: any) {
-  this.gatService.getCentersByName(event.option.value).subscribe(data => {
-    this.selectedOption15 = data.id;
-  });
-}
-
-onOptionSelected2(event: any) {
-  this.service.getPartnersById(event.option.value).subscribe(data => {
-    this.selectedOption2 = data.dni;
-    this.selectedOption3 = data.nombre;
-    this.selectedOption4 = data.nrocuenta;
-    this.selectedOption5 = data.localidad;
-  });
-  
-}
-
-onOptionSelected4(event: any) {
-  this.service.getPartnersByDni(event.option.value).subscribe(data => {
-    this.selectedOption17 = data.id;
-    this.selectedOption3 = data.nombre;
-    this.selectedOption4 = data.nrocuenta;
-    this.selectedOption5 = data.localidad;
-  });
-  
-}
-onOptionSelected5(event: any) {
-  this.service.getPartnersByName(event.option.value).subscribe(data => {
-    this.selectedOption17 = data.id;
-    this.selectedOption3 = data.nombre;
-    this.selectedOption2 = data.dni;
-    this.selectedOption4 = data.nrocuenta;
-    this.selectedOption5 = data.localidad;
-  });
-  
-}
-
-getData(id: string){
-  if(id === 'RFF'){
-    this.selectedOption6 = '164.00';
-    this.selectedOption7 = '603.028';
-    this.selectedOption8 = '164';
-    this.selectedOption9 = '0.000';
-    this.selectedOption10 = '0.000';
-    this.selectedOption11 = '603.028';
-    this.selectedOption12 = '0.000';
-    this.selectedOption13 = '0.000';
+    //FIN FORMULARIO
   }
-  else{
+
+  ngOnInit() {
+    this.loadOptions(); //CARGAR OPCIONES PARA AUTOCOMPLETE
+    this.getTickets() //CARGAR TODOS LOS TICKETS
+    this.list.controls['cantidad'].valueChanges.subscribe(() => {
+      this.updateTicket();
+    });
+  }
+
+  loadOptions() {
+    // Realiza la solicitud HTTP para obtener los datos del endpoint
+    this.gatService.getCenters().subscribe(data => {
+      // Mapea los datos de respuesta al formato de opciones que necesitas (id y value)
+      const options4 = data.map(item => item.centro);
+
+      // Configura la lógica de filtrado y asigna las opciones filtradas a la variable
+      this.filteredOptions4 = this.list.controls['centroacopio'].valueChanges.pipe(
+        startWith(''),
+        map(value => this.filterOptions(value, options4))
+      );
+    });
+
+    this.gatService.getTransportistas().subscribe(data => {
+      // Mapea los datos de respuesta al formato de opciones que necesitas (id y value)
+      const options3 = data.map(item => item.transporte);
+
+      // Configura la lógica de filtrado y asigna las opciones filtradas a la variable
+      this.filteredOptions3 = this.list.controls['transportista'].valueChanges.pipe(
+        startWith(''),
+        map(value => this.filterOptions(value, options3))
+      );
+    });
+
+    this.gatService.getCenters().subscribe(data => {
+      // Mapea los datos de respuesta al formato de opciones que necesitas (id y value)
+      const options = data.map(item => item.id);
+
+      // Configura la lógica de filtrado y asigna las opciones filtradas a la variable
+      this.filteredOptions = this.list.controls['codigoacopio'].valueChanges.pipe(
+        startWith(''),
+        map(value => this.filterOptions(value, options))
+      );
+    });
+
+    this.service.getPartners().subscribe(data => {
+      // Mapea los datos de respuesta al formato de opciones que necesitas (id y value)
+      const options2 = data.map(item => item.id);
+
+      // Configura la lógica de filtrado y asigna las opciones filtradas a la variable
+      this.filteredOptions2 = this.list.controls['codigosocio'].valueChanges.pipe(
+        startWith(''),
+        map(value => this.filterOptions(value, options2))
+      );
+    });
+    this.service.getPartners().subscribe(data => {
+      // Mapea los datos de respuesta al formato de opciones que necesitas (id y value)
+      const options5 = data.map(item => item.dni);
+
+      // Configura la lógica de filtrado y asigna las opciones filtradas a la variable
+      this.filteredOptions5 = this.list.controls['numerodoc'].valueChanges.pipe(
+        startWith(''),
+        map(value => this.filterOptions(value, options5))
+      );
+    });
+    this.service.getPartners().subscribe(data => {
+      // Mapea los datos de respuesta al formato de opciones que necesitas (id y value)
+      const options6 = data.map(item => item.nombre);
+
+      // Configura la lógica de filtrado y asigna las opciones filtradas a la variable
+      this.filteredOptions6 = this.list.controls['nombres'].valueChanges.pipe(
+        startWith(''),
+        map(value => this.filterOptions(value, options6))
+      );
+    });
+  }
+
+  filterOptions(value: string, options: string[]): string[] {
+    // Filtra las opciones basadas en el valor de entrada
+    const filterValue = value.toLowerCase();
+    return options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  onOptionSelected(event: any) {
+
+    this.gatService.getCentersById(event.option.value).subscribe(data => {
+      this.selectedOption = data.centro;
+
+    });
+  }
+  onOptionSelected3(event: any) {
+    this.gatService.getCentersByName(event.option.value).subscribe(data => {
+      this.selectedOption15 = data.id;
+    });
+  }
+
+  onOptionSelected2(event: any) {
+    this.service.getPartnersById(event.option.value).subscribe(data => {
+      this.selectedOption2 = data.dni;
+      this.selectedOption3 = data.nombre;
+      this.selectedOption4 = data.nrocuenta;
+      this.selectedOption5 = data.localidad;
+    });
 
   }
-}
+
+  onOptionSelected4(event: any) {
+    this.service.getPartnersByDni(event.option.value).subscribe(data => {
+      this.selectedOption17 = data.id;
+      this.selectedOption3 = data.nombre;
+      this.selectedOption4 = data.nrocuenta;
+      this.selectedOption5 = data.localidad;
+    });
+
+  }
+  onOptionSelected5(event: any) {
+    this.service.getPartnersByName(event.option.value).subscribe(data => {
+      this.selectedOption17 = data.id;
+      this.selectedOption3 = data.nombre;
+      this.selectedOption2 = data.dni;
+      this.selectedOption4 = data.nrocuenta;
+      this.selectedOption5 = data.localidad;
+    });
+
+  }
+  getTickets(){
+    this.ticket.getTickets().subscribe((tikckets) => {
+      this.dataSource2 = new MatTableDataSource<Ticket>(tikckets);
+    });
+  }
+
+  getData(id: string) {
+    if (id === 'RFF') {
+      const compra = this.list.controls['compra'].value;
+      const cantidad = this.list.controls['cantidad'].value;
+      this.precioDolarR = 164.0000;
+      this.precioDolar = 164.0000;
+      this.igvDolar = 0.0000;
+      this.igvSoles = 0.0000;
+      this.precioSolesR = compra * this.precioDolarR;
+      this.precioSoles = compra * this.precioDolarR;
+      this.importeDolares = this.precioDolar * cantidad + this.igvDolar;
+      this.importeSoles = this.precioSoles * cantidad + this.igvSoles;
+
+    }
+    else {
+      //TO DO: FUNCION PARA EL OTRO TIPO DE PLATA
+    }
+  }
+  updateTicket() {
+    const compra = this.list.controls['compra'].value;
+    const cantidad = this.list.controls['cantidad'].value;
+    this.precioSolesR = compra * this.precioDolarR;
+    this.precioSoles = compra * this.precioDolarR;
+    this.importeDolares = this.precioDolar * cantidad + this.igvDolar;
+    this.importeSoles = this.precioSoles * cantidad + this.igvSoles;
+  }
+  crearTicket(){
+  const compra = this.list.controls['moneda'].value;
+  let nevoTicket: Ticket; // Quitamos la inicialización como objeto vacío
+  if (compra === 'SOLES') {
+    nevoTicket = {
+      id: Math.random().toString(36).substr(2, 9),
+      ticket: this.list.controls['ticket'].value,
+      cantidad: this.list.controls['cantidad'].value, // Asigna el valor correspondiente
+      undmedida: this.list.controls['unidadm'].value,
+      descripcion: 'no c',
+      preciou: this.precioSoles,
+      igv: this.igvSoles,
+      descuento: '0.000',
+      importe: this.importeSoles
+    };
+  } else {
+    nevoTicket = {
+      id: Math.random().toString(36).substr(2, 9),
+      ticket: this.list.controls['ticket'].value,
+      cantidad: this.list.controls['cantidad'].value, // Asigna el valor correspondiente
+      undmedida: this.list.controls['unidadm'].value,
+      descripcion: 'no c',
+      preciou: this.precioDolar,
+      igv: this.igvDolar,
+      descuento: '0.000',
+      importe: this.importeDolares
+    };
+  }
+
+  this.ticket.createTicket(nevoTicket).subscribe(
+    response => {
+      this.getTickets();
+    },
+    error => {
+      console.error('Error al crear el ticket:', error);
+    }
+  );
+
+  
+  }
 
 }
